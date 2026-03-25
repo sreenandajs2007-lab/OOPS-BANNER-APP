f324acf1c72009002702a48f08355bfcd90c906f
sreenandajs2007-lab <sreenandajs2007@gmail.com>
2026-03-25 09:05:25 +0530

    UC1 Print OOPS to console

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+/**
+ * OOPS Banner App
+ * Goal: Display the literal text "OOPS" (Object-Oriented Programming System) once on the console.
+ * * @author Your Name
+ * @version 1.0
+ */
+public class OOPSBannerApp {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    /**
+     * Main Method - Entry point of the Java application.
+     * * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Using System.out.println() to display the String literal to the console
+        System.out.println("OOPS");
+
     }
 }
